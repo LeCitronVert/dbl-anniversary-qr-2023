341d1f816c2c8f1bb4e90bf0fc4a1350b9fa86eb
LeCitronVert <youcefphnixlord@gmail.com>
2023-06-17 12:06:52 +0200

    Adding automatic timestamp generation

--- qr.js
+++ qr.js
@@ -1,5 +1,36 @@
+class DblTimestampHelper{
+  static timestampEncoding = ['B','C','D','E','F','G','H','J','K','M','N','P','Q','R','S','T'];
+
+  static createDblTimestamp(){
+    let creationDate = Date.now().toString(16);
+    let encodedTimestamp = '';
+
+    for (let i = 0;i < creationDate.length;i++) {
+      encodedTimestamp += DblTimestampHelper.timestampEncoding[parseInt(creationDate[i], 16)];
+    }
+
+    return encodedTimestamp;
+  }
+
+  static decodeDblTimestamp(timestampString){
+    let decodedDblTimestamp = '';
+
+    for (let i = 0;i < timestampString.length;i++) {
+      decodedDblTimestamp += DblTimestampHelper.timestampEncoding.indexOf(timestampString[i]).toString(16);
+    }
+
+    return decodedDblTimestamp;
+  }
+}
+
+function getSelectedGenerationmethod(){
+   return document.querySelector('[name="qr-method"]:checked')?.value;
+}
+
 document.addEventListener("DOMContentLoaded", (event) => {
     const dblRegexPattern = new RegExp('^4,[a-zA-Z0-9]{8}');
+
+    const methodFieldset = document.querySelector('.qr-method');
 
     const formElement = document.querySelector('#dbl-qr');
     const fileField = document.querySelector('#qrcode');
@@ -46,8 +77,37 @@ document.addEventListener("DOMContentLoaded", (event) => {
         qrcode.decode(data)
     }
 
+    methodFieldset.addEventListener('change', (event) => {
+      fileField.toggleAttribute('required', 'manual' === getSelectedGenerationmethod());
+    });
+
     formElement.addEventListener('submit', (event) => {
       event.preventDefault();
+
+      DblTimestampHelper.createDblTimestamp();
+
+      if ('auto' === getSelectedGenerationmethod()) {
+        uglyAssBitch.forEach((friend) => {
+          if (0 === friend.input.value.length) {
+            return;
+          }
+
+          let newQr = '4,' + friend.input.value + DblTimestampHelper.createDblTimestamp();
+
+          friend.display.innerHTML = '';
+
+          let qrcode = new QRCode(friend.display, {
+              text: newQr,
+              width: 180, //128
+              height: 180,
+              colorDark: "#000000",
+              colorLight: "#ffffff",
+              correctLevel: QRCode.CorrectLevel.H
+          });
+        });
+        return;
+      }
+
       decodeImageFromBase64(qrField.value,function(decodedInformation){
           if (null === decodedInformation.match(dblRegexPattern)) {
             alert('Cant find your DBL info from image. Try cropping the image to only show the QR code.');
